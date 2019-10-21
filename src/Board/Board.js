174fb72742b92bbe9b043d72f9d9d32f9fc972a7
Vladut Muresan <vladmuresan02@gmail.com>
2019-10-22 01:29:13 +0300

    added code fopr bead to be written as two loops

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -10,21 +10,16 @@ class Board extends React.Component {
     render() {
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {
+                    [0, 1, 2].map(i => {
+                        return (
+                            <div className="board-row">
+                                {[0,1,2].map(j =>  this.renderSquare(3*i + j))}
+
+                            </div>
+                        );
+                    })
+                }
             </div>
         );
     }
