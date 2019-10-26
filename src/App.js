ca508b80a0d2a6cc825bf40c647a480aac6ba20d
Vladut Muresan <vladmuresan02@gmail.com>
2019-10-26 16:43:28 +0300

    Added draw message

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends React.Component {
         this.setState({
             history: history.concat([{
                 squares: squares,
-                move: `${squares[i]} to col ${(i % 3) + 1 } and row ${(Math.floor(i / 3) + 1)} `,
+                move: `${squares[i]} to col ${(i % 3) + 1} and row ${(Math.floor(i / 3) + 1)} `,
             }]),
             xIsNext: !this.state.xIsNext,
             stepNumber: history.length
@@ -44,20 +44,33 @@ class App extends React.Component {
 
     sortMoves() {
         this.setState({
-            movesAscendingOrder : !this.state.movesAscendingOrder
+            movesAscendingOrder: !this.state.movesAscendingOrder
         })
     }
+
+    composeStatus(result, current) {
+        console.log(result);
+        if (result) {
+            if (result === 'Draw!') {
+                return 'Draw!'
+            }
+            return `Winner: ${current.squares[result[0]]}`
+        }
+
+        return `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+    }
+
 
     render() {
         const history = this.state.history;
         const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
         let status;
-        status = winner ? `Winner: ${current.squares[winner[0]]}` : `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+        status = this.composeStatus(winner, current);
         let moves = history.map((step, move) => {
-            const desc = move ? `Go to move # ${move}` : 'Go to game start';
+            const desc = move ? `Go to move #${move}` : 'Go to game start';
             return (
-                <li key={move} className={move === this.state.stepNumber ?  'li-bolded'  : ''}>
+                <li key={move} className={move === this.state.stepNumber ? 'li-bolded' : ''}>
                     <button onClick={() => {
                         this.jumpToMove(move)
                     }}>{desc} -- {step.move} </button>
@@ -65,23 +78,24 @@ class App extends React.Component {
             );
         });
 
-        if(!this.state.movesAscendingOrder){
-            moves = moves.sort((a,b) => b.key - a.key)
+        if (!this.state.movesAscendingOrder) {
+            moves = moves.sort((a, b) => b.key - a.key)
         }
 
 
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board squares={current.squares} onClick={(i) => this.handleClick(i)}  win={winner ? winner : false}/>
+                    <Board squares={current.squares} onClick={(i) => this.handleClick(i)}
+                           win={winner ? winner : false}/>
                 </div>
                 <div className="game-info">
                     <div>
                         {status}
                     </div>
                     <ol>
-                        <button onClick={() => this.sortMoves() }>
-                            {this.state.movesAscendingOrder ? 'sort asc' : ' sort desc'}
+                        <button onClick={() => this.sortMoves()}>
+                            {this.state.movesAscendingOrder ? 'sort desc' : ' sort asc'}
                         </button>
                         {moves}
                     </ol>
@@ -92,6 +106,8 @@ class App extends React.Component {
 }
 
 function calculateWinner(squares) {
+
+
     const lines = [
         [0, 1, 2],
         [3, 4, 5],
@@ -102,12 +118,19 @@ function calculateWinner(squares) {
         [0, 4, 8],
         [2, 4, 6],
     ];
+
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
             return lines[i];
         }
     }
+
+    let nullValues = squares.filter(value => value === null);
+    if (nullValues.length === 0) {
+        return 'Draw!'
+    }
+
     return null;
 }
 
